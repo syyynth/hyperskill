ce854ec5e256f2bd0b139150cd5ae8ac6232bf8e
synth <102881855+syyynth@users.noreply.github.com>
2023-12-18 15:26:39 +0300

    update 213: Algoruthms with IMDB

--- python/0213 - Algorithms with IMDB/task/algorithms.py
+++ python/0213 - Algorithms with IMDB/task/algorithms.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def linear_search(movies, r):
     for title, rating in movies:
         if rating == r:
@@ -9,6 +12,8 @@ def bubble_sort(data):
         for j in range(len(data) - 1):
             if data[j][1] > data[j + 1][1]:
                 data[j], data[j + 1] = data[j + 1], data[j]
+    # this, or making merge sort in-place :x
+    return data
 
 
 def binary_search(movies, rating):
@@ -61,9 +66,31 @@ def print_all(movies):
         print(f'{title} - {rating}')
 
 
-with open('../movies.csv', encoding='U8') as f:
-    data = []
-    for movie in f:
-        title, rating = movie.strip().rsplit(',', maxsplit=1)
-        data.append((title.strip('"'), float(rating)))
-    binary_search(merge_sort(data), 6)
+def get_data(path):
+    with open(path, encoding='U8') as f:
+        data = []
+        for movie in f:
+            title, rating = movie.strip().rsplit(',', maxsplit=1)
+            data.append((title.strip('"'), float(rating)))
+        return data
+
+
+if len(sys.argv) == 1:
+    binary_search(merge_sort(get_data('movies.csv')), 6)
+else:
+    sort_algo = {
+        'bubble': bubble_sort,
+        'merge': merge_sort
+    }
+    search_algo = {
+        'linear': linear_search,
+        'binary': binary_search
+    }
+    try:
+        data = get_data(sys.argv[1])
+        _sort = sys.argv[2]
+        _search = sys.argv[3]
+        _rating = float(sys.argv[4])
+        search_algo[_search](sort_algo[_sort](data), _rating)
+    except:
+        print('Example of a run: python.exe algorithms.py movies.csv bubble binary 6.6')
